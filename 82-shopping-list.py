6d0b5b2d8cb71bb5219617cc8e1b812c557e6660
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-06-30 18:59:28 +0000

    Add files via upload

--- 82-shopping-list.py
+++ 82-shopping-list.py
@@ -8,6 +8,7 @@ def show_menu():
     print('Enter items you want to add to your shopping list')
     print('Press DONE to complete your order')
     print('Press REMOVE to remove an item from the cart')
+    print('Press SHOW to display your shopping list')
     print('Press HELP for assistance')
     print('-------------------------')
 
@@ -30,19 +31,49 @@ def remove_from_cart(item):
         print('Please you do not have that item in your cart')
 
 def display_items():
-    print('This is your shopping list')
-    for item in main_cart:
-        print(item)
+    if main_cart == []:
+        print('You do not have anything in your list')
+    else:
+        print('This is your shopping list')
+        for item in main_cart:
+            print(item)
 
+# main logic for the application
 show_menu()
-add_to_cart('mango')
-#remove_from_cart()
-add_to_cart('guava')
-add_to_cart('apples')
+while True:
+    user_input = input(' >  ')
+    if user_input == 'DONE':
+        show_menu()
+        print('Thanks for shopping with Foodie Mall ')
+        break
+    elif user_input == 'HELP':
+        show_menu()
+        print('Type to add an item to your shopping list')
+        continue
+    elif user_input == 'SHOW':
+        #show_menu()
+        display_items()
+    elif user_input == 'REMOVE':
+        display_items()
+        item_remove = input('Enter item to remove: ')
+        show_menu()
+        remove_from_cart(item_remove)
+        continue
+    else:
+        show_menu()
+        add_to_cart(user_input)
+
 display_items()
 
 
-#print(main_cart)
+# show_menu()
+# add_to_cart('mango')
+# remove_from_cart()
+# add_to_cart('guava')
+# add_to_cart('apples')
+# display_items()
+
+
 
 
 
